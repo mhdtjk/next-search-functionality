e06056097475b78c93865322f93a8341e02df40c
Mehrdad Tajik <37112957+mhdtjk@users.noreply.github.com>
2025-01-30 08:03:21 +0330

    Update search.ts

--- src/actions/search.ts
+++ src/actions/search.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export async function search(formData: FormData) {
     const term = formData.get('term')
 
-    if(term === "") redirect('/')
+    if(typeof term !== "string" || !term) redirect('/')
     
     redirect(`/search?term=${term}`)
 }
